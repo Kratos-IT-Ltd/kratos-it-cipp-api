2122e79e7c82dbb50519c688e771d6ac1b5dc656
KratosIT-Zach <zach@kratosit.co.uk>
2025-04-15 15:41:01 +0100

    Update Install.ps1

--- AddChocoApp/IntunePackage/Install.ps1
+++ AddChocoApp/IntunePackage/Install.ps1
@@ -17,21 +17,15 @@ param (
     $Trace
 )
 
-$KratosITPrivateRepo = "https://choco.kratoscloud.co.uk/nuget/kit-private-chocolatey/"
-$ChocoInstallScriptPath = "https://choco.kratoscloud.co.uk/endpoints/kit-private-assets/content/chocoinstall.ps1"
-$PrivateRepoUsername = "api"
-$PrivateRepoPassword = "{REDACTED}"
-$PrivateRepoCredentials = ('{0}:{1}' -f $PrivateRepoUsername, $PrivateRepoPassword)
-
 try {
     if ($Trace) { Start-Transcript -Path (Join-Path $env:windir "\temp\choco-$Packagename-trace.log") }
-    $chocoPath = "$($ENV:SystemDrive)\ProgramData\chocolatey\bin\choco.exe"
+    $chocoPath = "$($env:SystemDrive)\ProgramData\chocolatey\bin\choco.exe"
 
     if ($InstallChoco) {
         if (-not (Test-Path $chocoPath)) {
             try {
-                Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression (Invoke-WebRequest $ChocoInstallScriptPath -Headers @{"AUTHORIZATION"="Basic " + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($PrivateRepoCredentials))})
-                $chocoPath = "$($ENV:SystemDrive)\ProgramData\chocolatey\bin\choco.exe"
+                Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+                $chocoPath = "$($env:SystemDrive)\ProgramData\chocolatey\bin\choco.exe"
             }
             catch {
                 Write-Host "InstallChoco Error: $($_.Exception.Message)"
@@ -40,8 +34,8 @@ try {
     }
 
     try {
-        $localprograms = & "$chocoPath" list
-        $CustomRepoString = if ($CustomRepo) { "--source $KratosITPrivateRepo --user '$PrivateRepoUsername' --password '$PrivateRepoPassword'" } else { $null }
+        $localprograms = & "$chocoPath" list --localonly
+        $CustomRepoString = if ($CustomRepo) { "--source $customrepo" } else { $null }
         if ($localprograms -like "*$Packagename*" ) {
             Write-Host "Upgrading $packagename"
             & "$chocoPath" upgrade $Packagename $CustomRepoString
@@ -51,7 +45,7 @@ try {
             & "$chocoPath" install $Packagename -y $CustomRepoString
         }
         Write-Host 'Completed.'
-    }  
+    }
     catch {
         Write-Host "Install/upgrade error: $($_.Exception.Message)"
     }
